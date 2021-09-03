be13c647ba039af91cd8bf532968fc398fd3c89c
novacooo <jaceknowak226@gmail.com>
2021-09-03 18:20:27 +0200

    Changed DOM structure and css styles for development page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  // useEffect(() => {
-  //   document.title = "novaco.dev"
-  // }, []);
-
   return (
-    <div className="container">
-      <header className="header">
-        <h1>
-          novaco.dev
-        </h1>
-        <p>website is under development</p>
-      </header>
-    </div>
+    <>
+      <h1>novaco.dev</h1>
+      <p>website is under development</p>
+    </>
   );
 }
 
