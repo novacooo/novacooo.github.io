5fbd81f4521bb6b8b1f3e34d734d80d127b78eed
novacooo <jaceknowak226@gmail.com>
2021-09-06 19:32:27 +0200

    Implemented global styles

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = (): JSX.Element => (
   <>
-    <h1>novaco.dev</h1>
-    <p>website is under development</p>
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&display=swap"
+        rel="stylesheet"
+      />
+    </Helmet>
+    <GlobalStyle />
+    <div>
+      <h1>novaco.dev</h1>
+      <p>website is under development</p>
+    </div>
   </>
 );
 
